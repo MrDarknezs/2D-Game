e857910927b810bd09ca6360a8cb9fdd3edff33b
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-12-26 21:20:07 +0100

    Have made some changes

---
+++ Assets/LavaDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaDeath : MonoBehaviour {
+
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(player);
+        }
+    }
+}
